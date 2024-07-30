583f9ec7b1e311607dccc0697886eccdde2542b0
mughalkhan21 <142731648+mughalkhan21@users.noreply.github.com>
2024-07-31 00:39:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Find And Click The One With Different Colour");
+
+
+function newPage(){
+    alert("Wooohoooo You Find It!")
+}
+
+
---
+++ script2.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.body.style.backgroundImage = "url('images/bg.jpg')"; 
+    document.body.style.backgroundPosition = "center center"
+    document.body.style.backgroundSize="cover"
+    const element = document.getElementById("lbtn");
+    element.remove();
+  }
